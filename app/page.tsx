a2120f729647e4e0f81800876caad14e1357bd1b
John Munson <19480078+johncmunson@users.noreply.github.com>
2026-06-16 00:49:19 -0500

    small refactor

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import localFont from "next/font/local";
 import styles from "./home.module.css";
 import WealthExperience from "./wealth-experience";
 
-const domaineSans = localFont({
-  src: "./fonts/domaine-sans-display-thin-italic.woff2",
+const ampersandFont = localFont({
+  src: "./fonts/ampersand-font.woff2",
   weight: "100",
   style: "italic",
   display: "swap",
 });
 
 export default function Home() {
-  const ampersandClass = `${domaineSans.className} ${styles.ampersand}`;
+  const ampersandClass = `${ampersandFont.className} ${styles.ampersand}`;
 
   return (
     <main className={styles.page}>
